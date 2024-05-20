fcd2087357714e92b820af2705ef375ba9a529ce
setykarimi <setykarimi80@gmail.com>
2024-05-20 10:52:56 +0330

    Subject: Main page, Description: Edit content for main page, Date: 1403/02/31

--- App.js
+++ App.js
@@ -1,81 +1,38 @@
-import React, {useState} from 'react';
-import {Alert, Modal, StyleSheet, Text, Pressable, View, ActivityIndicator} from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
-const App = () => {
-  const [modalVisible, setModalVisible] = useState(false);
+export default function App() {
   return (
-    <View style={styles.centeredView}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
-        }}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Hello World!</Text>
-            <ActivityIndicator/>
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={() => setModalVisible(!modalVisible)}>
-              <Text style={styles.textStyle}>Hide Modal</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-      <Pressable
-        style={[styles.button, styles.buttonOpen]}
-        onPress={() => setModalVisible(true)}>
-        <Text style={styles.textStyle}>Show Modal</Text>
-      </Pressable>
+    <View style={styles.container}>
+      <View style={[styles.lightBlueBg, styles.box]}>
+        <Text>Lightblue box</Text>
+      </View>
+      <View style={[styles.lightBlueBg, styles.box]}>
+        <Text>Lightgreen box</Text>
+      </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  centeredView: {
+  container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
+    backgroundColor: "plum",
+    padding: 60,
   },
-  modalView: {
-    margin: 20,
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
+  box: {
+    width: 100,
+    height: 100,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    marginVertical: 10,
+    borderWidth: 2,
+    borderColor: 'purple',
+    borderRadius: 5
   },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
+  lightBlueBg: {
+    backgroundColor: "blue",
   },
-  buttonOpen: {
-    backgroundColor: '#F194FF',
-  },
-  buttonClose: {
-    backgroundColor: '#2196F3',
-  },
-  textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: 'center',
+  lightGreenBg: {
+    backgroundColor: "green",
   },
 });
-
-export default App;
