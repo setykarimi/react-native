0a58ed7ca75e1f759ed36563f9defa674057dd1e
setykarimi <setykarimi80@gmail.com>
2024-06-02 13:04:59 +0330

    Subject: Config, Description: Add new components, Date: 1403/03/13

--- App.js
+++ App.js
@@ -1,68 +1,25 @@
-import { StyleSheet, View } from "react-native";
-import Box from "./components/box";
+// import { StyleSheet, View } from "react-native";
+// import Box from "./components/box";
+
+import AlignContentBasics from "./components/align-content";
+import AlignItemsBasic from "./components/align-items";
+import AlignSelfBasics from "./components/align-self";
+import FlexBasisGrowShrink from "./components/flex-basis-grow-shrink";
+import PositionLayout from "./components/position";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Box style={{backgroundColor: "blue", alignSelf: "flex-start"}} />
-      <Box style={{backgroundColor: "red", alignSelf: "flex-end"}} />
-      <Box style={{backgroundColor: "green", alignSelf: "center"}} />
-      <Box style={{backgroundColor: "green", alignSelf: "stretch"}} />
-      <Box style={{backgroundColor: "lightGreen"}} />
-      <Box style={{backgroundColor: "lightRed"}} />
-      {/* <View style={styles.darkMode}>
-        <Text>Style inheritance</Text>
-      </View>
-      <View style={[styles.lightBlueBg, styles.box, styles.boxShadow]}>
-        <Text>Lightblue box</Text>
-      </View>
-      <View style={[styles.lightGreenBg, styles.box, styles.androidShadow]}>
-        <Text>Lightgreen box</Text>
-      </View> */}
-    </View>
+    <>
+      {/* <Box style={{ padding: 15, backgroundColor: "#221166" }} /> */}
+      {/* <Flex /> */}
+      {/* <FlexDirectionBasis /> */}
+      {/* <DirectionLayout /> */}
+      {/* <JustifyContentBasics /> */}
+      {/* <AlignItemsBasic /> */}
+      {/* <AlignSelfBasics /> */}
+      {/* <AlignContentBasics /> */}
+      {/* <FlexBasisGrowShrink /> */}
+      <PositionLayout />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "flex-center",
-    backgroundColor: "plum",
-    flexDirection: "column",
-    padding: 60,
-    alignSelf: "stretch",
-    flexWrap: "wrap-reverse"
-  },
-  darkMode: {
-    backgroundColor: "black",
-    color: "white",
-  },
-  box: {
-    width: 250,
-    height: 250,
-    paddingHorizontal: 10,
-    paddingVertical: 20,
-    marginVertical: 10,
-    borderWidth: 2,
-    borderColor: "purple",
-    borderRadius: 5,
-  },
-  lightBlueBg: {
-    backgroundColor: "blue",
-  },
-  lightGreenBg: {
-    backgroundColor: "green",
-  },
-  boxShadow: {
-    shadowColor: "#333333",
-    shadowOffset: {
-      width: 6,
-      height: 6,
-    },
-    shadowOpacity: 0.6,
-    shadowRadius: 4,
-  },
-  androidShadow: {
-    elevation: 10,
-  },
-});
