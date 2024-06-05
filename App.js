fea66f15838f406c9f801c9a8cd0c3a8903367bd
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-06-05 15:06:00 +0330

    Subject: React native, Description: Add Dimesions and Safe Area component, Date: 06/05/2024

--- App.js
+++ App.js
@@ -1,44 +1,10 @@
-// import { StyleSheet, View } from "react-native";
-import { StyleSheet, Text, View, useWindowDimensions } from "react-native";
+import SafeArea from "./components/safe-area";
 
 export default function App() {
-  const windowWidth = useWindowDimensions().width;
-  const windowHeight = useWindowDimensions().height;
-  // const [dimensions, setDimensions] = useState({
-  //   window: Dimensions.get("window"),
-  // });
-
-  // useEffect(() => {
-  //   const subsription = Dimensions.addEventListener("change", ({ window }) => {
-  //     setDimensions({ window });
-  //   });
-  //   return () => subsription?.remove();
-  // });
-
-  // const { window } = dimensions;
-
-  // const windowWidth = window.width;
-  // const windowHeight = window.height;
-
   return (
-    <View style={styles.container}>
-      <View
-        style={[
-          styles.box,
-          {
-            width: windowWidth > 500 ? "70%" : "90%",
-            height: windowHeight > 600 ? "60%" : "90%",
-          },
-        ]}
-      >
-        <Text
-          style={{
-            fontSize: windowWidth > 500 ? 50 : 24,
-          }}
-        >
-          Welcome!
-        </Text>
-      </View>
+    <>
+      <SafeArea />
+      {/* <Dimension /> */}
       {/* <Box style={{ padding: 15, backgroundColor: "#225544" }} >Box 1 shrink</Box>
       <Box style={{ padding: 15, backgroundColor: "#227766" }} >Box 2 shrink</Box>
       <Box style={{ padding: 15, backgroundColor: "#224433" }} >Box 3</Box>
@@ -56,25 +22,6 @@ export default function App() {
       {/* <AlignContentBasics /> */}
       {/* <FlexBasisGrowShrink /> */}
       {/* <PositionLayout /> */}
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "plum",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  box: {
-    // width: windowWidth > 500 ? "70%" : "90%",
-    // height: windowHeight > 600 ? "60%" : "90%",
-    backgroundColor: "lightblue",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  // text: {
-  //   fontSize: windowWidth > 500 ? 50 : 24,
-  // },
-});
