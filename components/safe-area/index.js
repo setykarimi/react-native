fb9d5d9f26477ea427d1a983c6481df87c8191db
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-06-05 15:05:41 +0330

    Subject: React native, Description: Create new component for SafeArea component, Date: 06/05/2024

---
+++ components/safe-area/index.js
@@ -0,0 +1,32 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+export default function SafeArea() {
+  return (
+    <SafeAreaView style={styles.safeContainer}>
+      <View style={styles.container}>
+        <View style={styles.box}>
+          <Text style={styles.text}>Welcome!</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+    backgroundColor: "plum",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "plum",
+  },
+  box: {
+    padding: 20,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
