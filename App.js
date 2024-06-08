5ac8941873ea81b3d7c2f0e88b33715e6bc5c4a3
setykarimi <setykarimi80@gmail.com>
2024-06-08 11:32:19 +0330

    Subject: React native, Description: Add pokeman, Date: 1403/03/19

--- App.js
+++ App.js
@@ -1,27 +1,15 @@
-import SafeArea from "./components/safe-area";
-import CustomButton from "./components/button";
+import { Platform, SafeAreaView, StyleSheet } from "react-native";
+import PokemanCard from "./components/pokeman/card";
 export default function App() {
-  return (
-    <>
-      <SafeArea />
-      {/* <Dimension /> */}
-      {/* <Box style={{ padding: 15, backgroundColor: "#225544" }} >Box 1 shrink</Box>
-      <Box style={{ padding: 15, backgroundColor: "#227766" }} >Box 2 shrink</Box>
-      <Box style={{ padding: 15, backgroundColor: "#224433" }} >Box 3</Box>
-      <Box style={{ padding: 15, backgroundColor: "#225555" }} >Box 4</Box>
-      <Box style={{ padding: 15, backgroundColor: "#225544" }} >Box 5</Box>
-      <Box style={{ padding: 15, backgroundColor: "#227766" }} >Box 6</Box>
-      <Box style={{ padding: 15, backgroundColor: "#224433" }} >Box 7</Box>
-      <Box style={{ padding: 15, backgroundColor: "#225555" }} >Box 8</Box> */}
-      {/* <Flex /> */}
-      {/* <FlexDirectionBasis /> */}
-      {/* <DirectionLayout /> */}
-      {/* <JustifyContentBasics /> */}
-      {/* <AlignItemsBasic /> */}
-      {/* <AlignSelfBasics /> */}
-      {/* <AlignContentBasics /> */}
-      {/* <FlexBasisGrowShrink /> */}
-      {/* <PositionLayout /> */}
-    </>
-  );
+  return <SafeAreaView style={styles.container}>
+    <PokemanCard />
+  </SafeAreaView>;
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+    paddingTop: Platform.OS == "android" ? 25 : 0
+  },
+});
