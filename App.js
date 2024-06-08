54d38cd82fde77432b132df4fa9ad908c43ce0f3
setykarimi <setykarimi80@gmail.com>
2024-06-08 11:58:44 +0330

    Subject: Pokeman, Description: Pass new data in app, Date: 1403/03/19

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import { Platform, SafeAreaView, StyleSheet } from "react-native";
 import PokemanCard from "./components/pokeman/card";
 export default function App() {
-  return <SafeAreaView style={styles.container}>
-    <PokemanCard />
-  </SafeAreaView>;
+  const charmanData = {
+    name: "Charmandar",
+    image: require("./assets/charmander.png"),
+    type: "Fire",
+    hp: 39,
+    moves: ["Scratch", "Ember", "Growl", "Leer"],
+    weaknesses: ["Water", "Rock"],
+  };
+  return (
+    <SafeAreaView style={styles.container}>
+      <PokemanCard {...charmanData} />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#f5f5f5",
-    paddingTop: Platform.OS == "android" ? 25 : 0
+    paddingTop: Platform.OS == "android" ? 25 : 0,
   },
 });
