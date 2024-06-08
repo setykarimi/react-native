2ce58f73b99adadc4fddef2fd34dff76e7f07cbf
setykarimi <setykarimi80@gmail.com>
2024-06-08 11:58:29 +0330

    Subject: Pokeman, Description: Edit for style, Date: 1403/03/19

--- components/pokeman/card.js
+++ components/pokeman/card.js
@@ -1,10 +1,51 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Platform, StyleSheet, Text, View } from "react-native";
 
-export default function PokemanCard() {
+export default function PokemanCard({
+  name,
+  image,
+  type,
+  hp,
+  moves,
+  weaknesses,
+}) {
   return (
-    <View>
-      <Text>Pokeman</Text>
+    <View style={styles.card}>
+      <View>
+        <Text>{name}</Text>
+        <Text>{hp}</Text>
+      </View>
+      <Image source={image} accessibilityLabel={`${name} pokeman`} />
+      <View>
+        <Text>{type}</Text>
+      </View>
+      <View>
+        <Text>Moves: {moves.join(",")}</Text>
+      </View>
+      <View>
+        <Text>Weaknesses: {weaknesses.join(",")}</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+    ...Platform.select({
+      ios: {
+        shadowOffset: { width: 2, height: 2 },
+        shadowColor: "#333",
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+});
