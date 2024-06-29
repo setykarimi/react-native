2800e1f4ba69c041f1628f567456aaeccf5836af
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-06-29 07:35:27 +0330

    Subject: Rn list, Description: Render RN list, Date: 6/29/2024

--- App.js
+++ App.js
@@ -1,18 +1,51 @@
-import { Platform, SafeAreaView, StyleSheet } from "react-native";
+import { Platform, SafeAreaView, ScrollView, StyleSheet } from "react-native";
 import PokemanCard from "./components/pokeman/card";
+import RnList from "./components/rn-list";
 export default function App() {
-  const charmanData = {
-    name: "Charmandar",
-    image: require("./assets/charmander.png"),
-    type: "Fire",
-    hp: 39,
-    moves: ["Scratch", "Ember", "Growl", "Leer"],
-    weaknesses: ["Water", "Rock"],
-  };
+  const data = [
+    {
+      name: "Charmandar",
+      image: require("./assets/charmander.png"),
+      type: "Fire",
+      hp: 39,
+      moves: ["Scratch", "Ember", "Growl", "Leer"],
+      weaknesses: ["Water", "Rock"],
+    },
+    {
+      name: "Squirtle",
+      image: require("./assets/squirtle.png"),
+      type: "Water",
+      hp: 44,
+      moves: ["Tackle", "Water Gun", "Tail whip", "Withdraw"],
+      weaknesses: ["Electric", "Grass"],
+    },
+    {
+      name: "Bulbasaur",
+      image: require("./assets/bulbasaur.png"),
+      type: "Grass",
+      hp: 45,
+      moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+      weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+    },
+    {
+      name: "Pikachu",
+      image: require("./assets/pikachu.png"),
+      type: "Electric",
+      hp: 49,
+      moves: ["Scratch", "Ember", "Growl", "Leer"],
+      weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+    },
+  ];
+
   return (
-    <SafeAreaView style={styles.container}>
-      <PokemanCard {...charmanData} />
-    </SafeAreaView>
+    <RnList />
+    // <SafeAreaView style={styles.container}>
+    //   <ScrollView>
+    //     {data.map((item) => (
+    //       <PokemanCard key={item.name} {...item} />
+    //     ))}
+    //   </ScrollView>
+    // </SafeAreaView>
   );
 }
 
