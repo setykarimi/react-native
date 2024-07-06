594f8312a1f7f6268a52cd498e83e81ddba34de5
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-07-06 06:43:43 +0330

    Subject: Posts List, Description: Add post handler and new post form, Date: 7/6/2024

--- components/posts-networking/index.js
+++ components/posts-networking/index.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import {
   ActivityIndicator,
+  Button,
   FlatList,
   SafeAreaView,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   View,
 } from "react-native";
 
@@ -13,6 +15,9 @@ export default function Posts() {
   const [postList, setPostList] = useState();
   const [isLoading, setIsLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
+  const [postTitle, setPostTitle] = useState(false);
+  const [postBody, setPostBody] = useState(false);
+  const [isPosting, setIsPosting] = useState(false);
 
   const fetchData = async (limit = 10) => {
     const response = await fetch(
@@ -24,9 +29,29 @@ export default function Posts() {
   };
 
   const handleRefresh = () => {
-    setRefreshing(true)
-    fetchData(20)
-    setRefreshing(false)
+    setRefreshing(true);
+    fetchData(20);
+    setRefreshing(false);
+  };
+
+  const addPost = async () => {
+    setIsPosting(true);
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: postTitle,
+        body: postBody,
+      }),
+    });
+
+    const newPost = await response.json()
+    setPostList([newPost,...postList])
+    setPostTitle("")
+    setPostBody("")
+    setIsPosting(false)
   };
 
   useEffect(() => {
@@ -44,29 +69,50 @@ export default function Posts() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.listContainer}>
-        <FlatList
-          renderItem={({ item }) => {
-            return (
-              <View style={styles.card}>
-                <Text style={styles.titleText}>{item.title}</Text>
-                <Text style={styles.bodyText}>{item.body}</Text>
-              </View>
-            );
-          }}
-          data={postList}
-          ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
-          ListEmptyComponent={<Text>No Posts Found</Text>}
-          ListHeaderComponent={
-            <Text style={styles.headerText}>Posts List</Text>
-          }
-          ListFooterComponent={
-            <Text style={styles.footerText}>End of list</Text>
-          }
-          refreshing={refreshing}
-          onRefresh={handleRefresh}
-        />
-      </View>
+      <>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Post title"
+            value={postTitle}
+            onChangeText={setPostTitle}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Post body"
+            value={postBody}
+            onChangeText={setPostBody}
+          />
+          <Button
+            title={isPosting ? "Adding ..." : "Add post"}
+            onPress={addPost}
+            disabled={isPosting}
+          />
+        </View>
+        <View style={styles.listContainer}>
+          <FlatList
+            renderItem={({ item }) => {
+              return (
+                <View style={styles.card}>
+                  <Text style={styles.titleText}>{item.title}</Text>
+                  <Text style={styles.bodyText}>{item.body}</Text>
+                </View>
+              );
+            }}
+            data={postList}
+            ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+            ListEmptyComponent={<Text>No Posts Found</Text>}
+            ListHeaderComponent={
+              <Text style={styles.headerText}>Posts List</Text>
+            }
+            ListFooterComponent={
+              <Text style={styles.footerText}>End of list</Text>
+            }
+            refreshing={refreshing}
+            onRefresh={handleRefresh}
+          />
+        </View>
+      </>
     </SafeAreaView>
   );
 }
@@ -111,5 +157,20 @@ const styles = StyleSheet.create({
     paddingTop: StatusBar.currentHeight,
     justifyContent: "center",
     alignItems: "center",
+  },
+  inputContainer: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    margin: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 8,
+    padding: 8,
+    borderRadius: 8,
   },
 });
