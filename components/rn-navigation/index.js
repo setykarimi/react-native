4a2d7a312aac77db43bbd62224cc3cc4064e4bbf
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-07-07 06:57:35 +0330

    Subject: Rn navigation, Description: Create new component for rn navigation, Date: 7/7/2024

---
+++ components/rn-navigation/index.js
@@ -0,0 +1,8 @@
+import { NavigationContainer } from "@react-navigation/native";
+import * as React from "react";
+
+export default function RnNavigation() {
+  return (
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+  );
+}
