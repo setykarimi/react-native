9ed4f1f2bbab30f8023f505814b19abba7dec59a
setykarimi <setykarimi80@gmail.com>
2024-07-08 16:30:45 +0330

    Subject: Rn navigation, Description: Add button for change route, Date: 7/8/2024

--- screens/home-screen.js
+++ screens/home-screen.js
@@ -1,10 +1,16 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
+      <Button
+        title="Go to about"
+        onPress={() => navigation.navigate("About")}
+      />
     </View>
   );
 }
