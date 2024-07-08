dfdcbfe2abe7f7b1a1142edd989e70a34e7802dd
setykarimi <setykarimi80@gmail.com>
2024-07-08 16:39:15 +0330

    Subject: Rn navigation, Description: Add navigate and route data, Date: 7/8/2024

--- screens/about-screen.js
+++ screens/about-screen.js
@@ -1,16 +1,26 @@
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function AboutScreen() {
   const navigation = useNavigation();
+  const route = useRoute()
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>About Screen</Text>
+      <Text style={styles.text}>name: {route.params.name}</Text>
       <Button
-        title="Go to home"
-        onPress={() => navigation.navigate("Home")}
+        title="Update the name"
+        onPress={() => navigation.setParams({
+          name: "Codevolution"
+        })}
+      />
+      <Button
+        title="Go back with data"
+        onPress={() => navigation.navigate("Home",{
+          result: "Data from about"
+        })}
       />
     </View>
   );
