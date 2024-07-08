ca6d04a2d69bb6acb96d71370eadde0881a2c349
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-07-08 06:55:52 +0330

    Subject: Rn navigation, Description: Create new folder for screens and add screens gto rn navigation - install new package, Date: 7/8/2024

--- components/rn-navigation/index.js
+++ components/rn-navigation/index.js
@@ -1,8 +1,18 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as React from "react";
+import HomeScreen from "../../screens/home-screen";
+import AboutScreen from "../../screens/about-screen";
+
+const Stack = createNativeStackNavigator();
 
 export default function RnNavigation() {
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
