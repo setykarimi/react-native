746f2a8b51b922638f91fbb8639aad0dbcb7c877
setykarimi <setykarimi80@gmail.com>
2024-07-09 17:22:17 +0330

    Subject: Rn navigation, Description: Add options in tab navigation, Date: 7/9/2024

--- components/bottom-navigation/index.js
+++ components/bottom-navigation/index.js
@@ -4,15 +4,25 @@ import React from "react";
 import CourseListScreen from "../../screens/course-list-screen";
 import ProfileScreen from "../../screens/profile-screen";
 import SettingScreen from "../../screens/setting-screen";
+import Ionicons from '@expo/vector-icons/Ionicons'
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomNavigation() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={{
+        tabBarLabelPosition: 'below-icon',
+        tabBarShowLabel: true,
+        tabBarActiveTintColor:'purple',
+        tabBarInactiveTintColor: '#000',
+      }}>
         <Tab.Screen name="Course list" component={CourseListScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} options={{
+          tabBarLabel: "my profile",
+          tabBarIcon: ({color})=> <Ionicons name="person" size={20} color={color}/>,
+          tabBarBadge: 2
+        }}/>
         <Tab.Screen name="Settings" component={SettingScreen} />
       </Tab.Navigator>
     </NavigationContainer>
