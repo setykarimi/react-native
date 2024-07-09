db49dee5edc3c64e916c584883f0abf55f1ffac2
setykarimi <setykarimi80@gmail.com>
2024-07-09 17:04:17 +0330

    Subject: Rn navigation, Description: Add options to gesture, Date: 7/9/2024

--- components/gesture-handler/index.js
+++ components/gesture-handler/index.js
@@ -1,18 +1,30 @@
-import 'react-native-gesture-handler'
-import { NavigationContainer } from '@react-navigation/native'
-import { createDrawerNavigator } from '@react-navigation/drawer'
-import DashboardScreen from '../../screens/dashboard-screen'
-import SettingScreen from '../../screens/setting-screen'
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import "react-native-gesture-handler";
+import DashboardScreen from "../../screens/dashboard-screen";
+import SettingScreen from "../../screens/setting-screen";
 
-const Drawer = createDrawerNavigator()
+const Drawer = createDrawerNavigator();
 
-export default function GestureHandler(){
-    return(
-        <NavigationContainer>
-            <Drawer.Navigator>
-                <Drawer.Screen name="Dashboard" component={DashboardScreen}/>
-                <Drawer.Screen name="Setting" component={SettingScreen}/>
-            </Drawer.Navigator>
-        </NavigationContainer>
-    )
+export default function GestureHandler() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen
+          name="Dashboard"
+          component={DashboardScreen}
+          options={{
+            title: "My dashboard",
+            drawerLabel: "Dashboard label",
+            drawerActiveTintColor: "#333",
+            drawerActiveBackgroundColor: 'lightblue',
+            drawerContentStyle:{
+                backgroundColor: "#c6cbef"
+            }
+          }}
+        />
+        <Drawer.Screen name="Setting" component={SettingScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
 }
